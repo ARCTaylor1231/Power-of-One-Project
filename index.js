e5545b90877e2401f592f8917405bf8acbb340f8
unknown <xFALLENx1231@gmail.com>
2020-05-05 02:15:05 -0700

    more updates

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Datastore = require('nedb');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config();
 app.listen(port, () => console.log('listening at ' + port));
 app.use(express.static('Public'));
 app.use(express.json());
